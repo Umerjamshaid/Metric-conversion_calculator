391f08b0b8b9ef67d37b532cb116169457fd7385
Umerjamshaid <139961276+Umerjamshaid@users.noreply.github.com>
2023-12-10 01:46:36 +0500

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 function w3_open() {
     document.getElementById("main").style.marginLeft = "15%";
-    document.getElementById("mySidebar").style.width = "15%";
+    document.getElementById("mySidebar").style.width = "30%";
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("openNav").style.display = 'none';
   }
